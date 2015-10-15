e4420eddea8e25e45760ccfd2426aaff8c88716a
Alex Minkoff <minkoff@gmail.com>
2015-10-15 06:05:56 -0400

    Add Bill Blass welcome email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,10 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+        @base_url = Rails.application.config.action_mailer.default_url_options
+        @ref_url = @base_url + '?ref=' + @user.referral_code
+        @share_message = "Hey friends! Sign up for Bill Blass's private launch offer, and get entered to win $3000 of product."
 
-        mail(:to => user.email, :subject => "Thanks for signing up!")
+        mail(:to => user.email, :subject => "Welcome to billblass.com")
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Prelaunchr::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = "http://eels.biz:3000"
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
     :address => "smtp.mailgun.org",
